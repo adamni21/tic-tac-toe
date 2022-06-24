69f288e130d03ff7ab65e0c2c01127654ae15edf
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-06-24 16:12:55 +0100

    improved displaying long names

--- src/components/Scoreboard.svelte
+++ src/components/Scoreboard.svelte
@@ -5,43 +5,38 @@
   export let player2Score: string | number;
 </script>
 
-<div class="main">
-  <div class="left details">
-    <div class="player1">{player1Name}</div>
-    <div class="score">{player1Score}</div>
+<div class="scoreboard">
+  <div class="player1" class:smaller={player1Name.length > 14}>
+    {player1Name}
   </div>
-  <!-- <div class="score"> - </div> -->
-  <div class="right details">
-    <div class="player2">{player2Name}</div>
-    <div class="score">{player2Score}</div>
+  <div class="player2" class:smaller={player2Name.length > 14}>
+    {player2Name}
   </div>
+  <div class="score">{player1Score}</div>
+  <div class="score">{player2Score}</div>
 </div>
 
 <style>
-  .main {
+  .scoreboard {
     padding-top: 1rem;
     width: 100%;
     font-size: 2rem;
-    display: flex;
+    text-align: center;
+    display: grid;
+    grid-template-columns: repeat(2, 45%);
+    justify-items: center;
+    align-items: center;
     column-gap: 10%;
-    justify-content: center;
-    align-items: center;
-  }
-  /* .right {
-    align-items: flex-end;
-  }
-  .left {
-    align-items: flex-start;
-  } */
-  .details {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex: 1;
-    max-width: 45%;
   }
   .score {
     font-size: 5rem;
+  }
+  .smaller {
+    font-size: 1.5rem;
+  }
+  .player1,
+  .player2 {
+    word-break: break-word;
   }
   .player1 {
     color: var(--secondary);
