83abf11d0df3639e366926ba19b3e2b62bcb5e55
adamni21 <85580454+adamni21@users.noreply.github.com>
2022-06-29 16:30:15 +0100

    ConfirmModal changed handler from event to prop funtions

--- src/components/UI/ConfirmModal.svelte
+++ src/components/UI/ConfirmModal.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
   import Button from "./Button.svelte";
   import Modal from "./Modal.svelte";
 
   export let isOpen: boolean;
-
-  const dispatch = createEventDispatcher();
-
-  const confirmHandler = () => dispatch("confirm");
-  const cancelHandler = () => dispatch("cancel");
+  export let confirmHandler;
+  export let cancelHandler;
 </script>
 
 <Modal {isOpen}>
   <div class="main rnd-corners">
     <div class="message">
-     <p><slot name="message" /></p>
+      <p><slot name="message" /></p>
     </div>
     <div class="actions">
       <Button class="action-btn" on:click={cancelHandler} outlined>
@@ -46,14 +42,14 @@
   }
   .message > p {
     margin: 0;
-    padding: 0 .5em;
+    padding: 0 0.5em;
   }
   .actions {
     grid-area: actions;
     justify-self: end;
   }
   .main :global(.action-btn) {
-    padding: .3rem .5rem;
+    padding: 0.3rem 0.5rem;
     border-radius: 99999px;
   }
 </style>
